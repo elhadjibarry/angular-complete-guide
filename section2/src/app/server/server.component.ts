838ff1854e16538e4211345df6d3f60ae7f6e541
Elhadji Barry <elhadji.barry@gmail.com>
2020-11-22 18:27:05 +0000

    Section 2 - One way databinding

--- section2/src/app/server/server.component.ts
+++ section2/src/app/server/server.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus() { 
+    return this.serverStatus;
+  }
+
   constructor() { }
   ngOnInit() { }
 }
--- section2/src/app/servers/servers.component.ts
+++ section2/src/app/servers/servers.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    //console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
