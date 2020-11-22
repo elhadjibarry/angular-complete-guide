e1373ddcf479e67f79c2ea22fe3adb2b8ea3fbdb
Elhadji Barry <elhadji.barry@gmail.com>
2020-11-22 15:21:29 +0000

    section 2 before upgarde to Angular 11

---
+++ section2/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit() { }
+}
