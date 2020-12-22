4af6a5430c66c47bc60b7be867316325c51dd3d2
Elhadji Barry <elhadji.barry@gmail.com>
2020-12-22 15:07:18 +0000

    section 5: Components and databinding Deep Dive

--- section5-component-databinding/src/app/server-element/server-element.component.ts
+++ section5-component-databinding/src/app/server-element/server-element.component.ts
@@ -1,17 +1,53 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated  //None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  @Input('serverElement') element: {type: string, name: string, content: string};
+  @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
 
-  constructor() { }
+  constructor() { 
+    console.log('constructor called');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
 }
